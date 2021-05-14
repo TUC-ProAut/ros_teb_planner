a9fabb4ebee4765f7d58806795c07cf9324fd7f4
Peter Weissig <peterweissig@github.com>
2021-05-14 10:32:57 +0200

    adapted plot_velocity_profil.m to critical corners

--- teb_planner_pa_matlab/scripts/+helper/plot_velocity_profil.m
+++ teb_planner_pa_matlab/scripts/+helper/plot_velocity_profil.m
@@ -57,10 +57,10 @@ catch exp    % Error from rosnode list
 end
 
 %% run as subfunction (to avoid polluting the workspace)
-plot_vel__sub(tebplan.getResultFeedback())
+plot_vel__sub(tebplan.getResultFeedback(), tebplan.criticalCorners())
 
 
-function plot_vel__sub(msg)
+function plot_vel__sub(msg, cc)
 
 
     %% check msg
@@ -71,7 +71,24 @@ function plot_vel__sub(msg)
 
     %% get data
     % const - rosinit is needed for rosparam to work
+    try
+        robot_radius = rosparam('get', '/teb_planner_node_pa/footprint_model/radius');
+    catch exp
+        robot_radius = 0.4;
+    end
+
     robot_vmax   = rosparam('get', '/teb_planner_node_pa/max_vel_x');
+    cc_dist      = rosparam('get', '/teb_planner_node_pa/critical_corner_dist');
+    cc_vel       = rosparam('get', '/teb_planner_node_pa/critical_corner_vel');
+    cc_dist_incl = rosparam('get', '/teb_planner_node_pa/critical_corner_inclusion_dist');
+
+    % convert data
+    if (nargin < 2)
+        cc = [];
+    end
+    if (isstruct(cc))
+        cc       = cell2mat({cc.x; cc.y})';
+    end
 
     %% calculate
     % at the given pose
@@ -87,12 +104,54 @@ function plot_vel__sub(msg)
     t_middle   = (  t(1:(end-1), : )  +   t(2:end, : )) / 2;
     l_middle   = (  l(1:(end-1), : )  +   l(2:end, : )) / 2;
 
+    % distance between poses and CC
+    % (also considering footprint of robot - simplified as radius)
+    dist_cc = zeros(size(pos, 1), size(cc, 1));
+    for icc = 1:size(cc, 1)
+        dist_cc(:, icc) = vecnorm(pos - cc(icc, :), 2, 2) - robot_radius;
+    end
+
+    % use closest pose (current or next)
+    %   (this is done within the preselection and the edge itself)
+    temp        = dist_cc((1:end-1), :);
+    temp(:,:,2) = dist_cc( 2:end   , :);
+    dist_cc     = min(temp, [], 3);
+
+    % only consider pose, if close enough
+    %   (this is done within the preselection)
+    mask = dist_cc < cc_dist_incl;
+    dist_cc(~mask) = inf;
+
+    % convert to scale
+    %   (this is done within the edge itself)
+    temp = dist_cc / cc_dist;
+    % apply quadratic scaling is further away
+    mask = temp > 1;
+    temp(mask) = temp(mask) .^ 2;
+
+    % convert to velocities
+    temp = temp * cc_vel;
+    % limit velocities to v_max
+    mask = temp > robot_vmax;
+    temp(mask) = robot_vmax;
+    vmax = temp;
+
+    % get min velocity for each pose
+    vmax = min(vmax, [], 2);
+    if (isempty(vmax))
+        vmax = repmat(robot_vmax, length(l)-1);
+    end
+
     %% plot results
     subplot(1,2,1)
     cla
     hold on
     plot(pos(:,1), pos(:,2), 'kx', 'DisplayName', 'reference points')
     plot(pos(:,1), pos(:,2), 'g-', 'DisplayName', 'path')
+    if (~isempty(cc))
+        plot(cc(:,1), cc(:,2), 'bo', 'MarkerSize', 5, ...
+          'DisplayName', 'critical corners')
+    end
 
     legend()
     xlabel('x in m')
@@ -117,8 +176,8 @@ function plot_vel__sub(msg)
     hold on
     plot(l(1:(end-1)), vel, 'kx')
     plot(l_middle, vel, 'b-', 'LineWidth', 3)
-    plot(l(1:(end-1)), repmat(robot_vmax, length(l) - 1, 1), 'r-')
-    legend('@reference point', 'plotted at midpoints', 'vmax_{global}')
+    plot(l(1:(end-1)), vmax, 'r-')
+    legend('@reference point', 'plotted at midpoints', 'vmax_{circular robot}')
     xlabel('rel. position in m')
     ylabel('velocity in m/s')
     title('velocity profil')
